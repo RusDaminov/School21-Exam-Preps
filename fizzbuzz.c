65299fbd555c7027efd73b241a91c207a3c1d476
abernita <abernita@ge-g6.kzn.21-school.ru>
2021-10-25 12:07:38 +0300

    +fizzbuzz

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,36 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abernita <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/28 21:54:53 by abernita          #+#    #+#             */
-/*   Updated: 2021/10/25 11:14:34 by abernita         ###   ########.fr       */
+/*   Created: 2021/10/25 11:13:39 by abernita          #+#    #+#             */
+/*   Updated: 2021/10/25 11:13:46 by abernita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <unistd.h>
 
-void	ft_putchar(char c)
+void    ft_putnbr(int n)
 {
+	int c;
+	c = 0;
+
+	if (n / 10 > 0)
+		ft_putnbr(n / 10);
+	c = n % 10 + 48;
 	write(1, &c, 1);
 }
 
-void	ft_print_alphabet(void)
+int main(void)
 {
-	int  i;
-	char  c;
+	int i;
+	char *s;
 
-	i = 97;
-	while (i <= 122)
+	i = 1;
+	while (i <= 100)
 	{
-        ft_putchar(c);
+		if (i % 15 == 0)
+		{
+			write(1, "FizzBuzz\n", 9);
+		} else if (i % 5 == 0) {
+			write(1, "Buzz\n", 5);
+		} else if (i % 3 == 0){
+			write(1, "Fizz\n", 5);
+		} else {
+			ft_putnbr(i);
+			write(1, "\n", 1);
+		}
 		i++;
 	}
-}
-
-int	main(void)
-{
-	ft_print_alphabet();
-	return (0);
+	return 0;
 }
